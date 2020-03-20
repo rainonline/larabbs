61ee0034cb8104b445edcde25ff594446b702996
rainonline <haoyuhy@yeah.net>
2020-03-20 17:08:27 +0800

    编辑个人资料

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -15,7 +15,8 @@
               <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
 
               <div class="col-md-6">
-                <input id="name" type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" value="{{ old('name') }}" required autofocus>
+                <input id="name" type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}"
+                  name="name" value="{{ old('name') }}" required autofocus>
 
                 @if ($errors->has('name'))
                 <span class="invalid-feedback" role="alert">
@@ -29,7 +30,8 @@
               <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
               <div class="col-md-6">
-                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
+                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}"
+                  name="email" value="{{ old('email') }}" required>
 
                 @if ($errors->has('email'))
                 <span class="invalid-feedback" role="alert">
@@ -43,18 +45,20 @@
               <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
               <div class="col-md-6">
-                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
+                <input id="password" type="password"
+                  class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
 
                 @if ($errors->has('password'))
-                  <span class="invalid-feedback" role="alert">
-                    <strong>{{ $errors->first('password') }}</strong>
-                  </span>
+                <span class="invalid-feedback" role="alert">
+                  <strong>{{ $errors->first('password') }}</strong>
+                </span>
                 @endif
               </div>
             </div>
 
             <div class="form-group row">
-              <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
+              <label for="password-confirm"
+                class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
 
               <div class="col-md-6">
                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
@@ -65,14 +69,16 @@
               <label for="captcha" class="col-md-4 col-form-label text-md-right">验证码</label>
 
               <div class="col-md-6">
-                <input id="captcha" class="form-control{{ $errors->has('captcha') ? ' is-invalid' : '' }}" name="captcha" required>
+                <input id="captcha" class="form-control{{ $errors->has('captcha') ? ' is-invalid' : '' }}"
+                  name="captcha" required>
 
-                <img class="thumbnail captcha mt-3 mb-2" src="{{ captcha_src('flat') }}" onclick="this.src='/captcha/flat?'+Math.random()" title="点击图片重新获取验证码">
+                <img class="thumbnail captcha mt-3 mb-2" src="{{ captcha_src('flat') }}"
+                  onclick="this.src='/captcha/flat?'+Math.random()" title="点击图片重新获取验证码">
 
                 @if ($errors->has('captcha'))
-                  <span class="invalid-feedback" role="alert">
-                    <strong>{{ $errors->first('captcha') }}</strong>
-                  </span>
+                <span class="invalid-feedback" role="alert">
+                  <strong>{{ $errors->first('captcha') }}</strong>
+                </span>
                 @endif
               </div>
             </div>
